b9cac0e5d911908f86e895f81420693d3e5b6287
MayankJaiswal1 <mayankjaiswalofficial1@gmail.com>
2024-07-30 22:22:36 +0530

    Project Submission

--- future_forge/src/main/java/com/invest_elevate/Main.java
+++ future_forge/src/main/java/com/invest_elevate/Main.java
@@ -2,6 +2,8 @@ package com.invest_elevate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Elevating Generatings through Investments");   
+        System.out.println("Team : Future Forge");
+        System.out.println("Project: Invest Elevate");
     }
 }
